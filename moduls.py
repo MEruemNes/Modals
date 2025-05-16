a25f8be5151b679af8ea0611b19e95902d9665c0
ENES TEKEŞ <enestekes01@gmail.com>
2025-05-16 11:15:46 +0300

    Add files via upload

--- moduls.py
+++ moduls.py
@@ -1,15 +1,21 @@
+import questionary
 import webbrowser
 
+secim = questionary.select(
+    "MODÜL SEÇİMİ YAPINIZ",
+    choices=[
+        "1 - A1",
+        "2 - A2",
+        "3 - B1",
+        "4 - B2"
+    ]
+).ask()
 
-print('MODÜL SEÇİMİ YAPINIZ\n 1=A1 \n 2=A2 \n 3=B1 \n 4=B2')
-
-modul = input("Modülünüzü giriniz(1,2,3,4): ")
-
-if modul == "1":
-     webbrowser.open("https://calculator-jctqjn5zjrrasjtf2jw5wi.streamlit.app/")
-elif modul == "2":
-     webbrowser.open("https://calculator-jctqjn5zjrrasjtf2jw5wi.streamlit.app/")
-elif modul == "3":
-     webbrowser.open("https://calculator-jctqjn5zjrrasjtf2jw5wi.streamlit.app/")
-else:
-     webbrowser.open("https://calculator-jctqjn5zjrrasjtf2jw5wi.streamlit.app/")
+if secim == "1 - A1":
+    webbrowser.open("https://example.com/A1")
+elif secim == "2 - A2":
+    webbrowser.open("https://example.com/A2")
+elif secim == "3 - B1":
+    webbrowser.open("https://calculator-jctqjn5zjrrasjtf2jw5wi.streamlit.app/")
+elif secim == "4 - B2":
+    webbrowser.open("https://example.com/B2")
